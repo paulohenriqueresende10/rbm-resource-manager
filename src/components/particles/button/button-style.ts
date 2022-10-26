5d8c9d8ad7a4f0706d7b856f6ba92726fdcac47e
p.resende <paulo.resende@rbmweb.com.br>
2022-10-26 20:50:29 -0300

    feat: adicionando cursor pointer no botao

--- src/components/particles/button/button-style.ts
+++ src/components/particles/button/button-style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { THEME } from "contants";
 
 export const StyledButton = styled.button`
+  cursor: pointer;
   background-color: ${THEME.light.colors.primary2};
   width: 410px;
   min-width: 320px;
--- src/components/particles/input/input-style.ts
+++ src/components/particles/input/input-style.ts
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const StyledInput = styled.input`
     width: 410px;
-    /* height: 54px; */
+    min-width: 320px;
     border: 1px solid #DCDDE3;
     border-radius: 6px;
     font-size: 14px;
     padding: 18px 16px 19px 16px;
+
     &:focus{
         border: 1px solid #0092D4;
         outline: none;
