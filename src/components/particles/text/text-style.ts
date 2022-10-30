57df93fb0ddb3d7c312160e280e3685b0bfbd290
paulohenriqueresende10 <paulo-henrique_1993@hotmail.com>
2022-10-29 21:15:14 -0300

    hotfix: ajustes na font-size

--- src/components/particles/text/text-style.ts
+++ src/components/particles/text/text-style.ts
@@ -3,7 +3,7 @@ import { TextProps } from 'components/particles/text/text-index'
 
 export const StyledText = styled.span<TextProps>`
     color: ${({color}) => color};
-	font-size: clamp(30px, 25vw,${({fontSize}) => fontSize});
+	font-size: ${({fontSize}) => fontSize};
 	font-weight: ${({fontWeight}) => fontWeight};
 	text-decoration: ${({decoration}) => decoration};
 	margin: ${({margin}) => margin};
